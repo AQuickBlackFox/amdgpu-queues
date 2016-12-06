3c430d5dc4f9a5993c5dc910fe79214855d0cee1
Aditya Atluri <Aditya.Atluri@amd.com>
2016-12-05 20:41:44 -0600

    added functionality for mem tracker

--- include/detail/hawkInit.h
+++ include/detail/hawkInit.h
@@ -1,14 +1,61 @@
 #pragma once
 
 #include <vector>
+#include <map>
+#include <list>
+#include <algorithm>
+
 #include <hsa/hsa.h>
+#include <hsa/hsa_ext_amd.h>
 
 namespace owl {
+
+  class snowy {
+    public:
+    hsa_agent_t agent;
+    std::vector<hsa_region_t> kern_arg;
+    std::vector<hsa_region_t> fine_grain;
+    std::vector<hsa_amd_memory_pool_t> pools;
+    snowy(hsa_agent_t _agent);
+    ~snowy() {}
+  };
+
+  class horned {
+    public:
+    std::map<uint32_t, std::list<void*>> cpuPtrMap;
+    std::map<uint32_t, std::list<void*>> gpuPtrMap;
+    std::vector<size_t> gpuSize;
+    std::vector<size_t> cpuSize;
+    void addCpuPtr(uint32_t cpuID, void* cpuPtr);
+    void addGpuPtr(uint32_t gpuID, void* gpuPtr);
+    void printCpuPtr();
+    void printGpuPtr();
+    horned() {}
+    ~horned();
+  };
+
+  class screech {
+    
+  };
+
   class hawk {
     private:
-      hsa_status_t getGPUs();
+      std::vector<owl::snowy> gpus;
+      std::vector<owl::snowy> cpus;
+      uint32_t numGPUs;
+      uint32_t numCPUs;
+      owl::horned memTracker;
+      hsa_status_t findGPUs();
+      hsa_status_t findCPUs();
+      hsa_status_t findAgentAccessPattern();
+      hsa_status_t findPoolAccessPattern();
     public:
       hawk(int);
-      ~hawk();
+      void *memAllocGPU(uint32_t gpuID, size_t size);
+      void *memAllocCPU(uint32_t cpuID, size_t size);
+      uint32_t memCpyCpuToGpu(void *dst, void *src, size_t size);
+      uint32_t getGpuIdByPtr(void *ptr);
+      uint32_t getCpuIdByPtr(void *ptr);
+      ~hawk() ;
   };
 }
--- test/test1.cpp
+++ test/test1.cpp
@@ -1,5 +1,14 @@
 #include"hawkInit.h"
+#include<iostream>
 
 int main() {
 	owl::hawk h1(1);
+  void *ptr_h, *ptr_d;
+  ptr_h = h1.memAllocCPU(0, 1024);
+  ptr_d = h1.memAllocGPU(0, 1024);
+  void *ptr_h1 = h1.memAllocCPU(1, 1024);
+  std::cout<<(uint64_t)ptr_h<<std::endl;
+  std::cout<<(uint64_t)ptr_d<<std::endl;
+  std::cout<<h1.memCpyCpuToGpu(ptr_d, ptr_h, 1024)<<std::endl;
+  std::cout<<h1.memCpyCpuToGpu(ptr_d, ptr_h1, 1024)<<std::endl;
 }
